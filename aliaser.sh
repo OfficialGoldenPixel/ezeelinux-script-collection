405806536eb7c87594adc713c9aa95ba3c1f5503
GoldenPixel <43580577+OfficialGoldenPixel@users.noreply.github.com>
2020-01-27 10:40:25 +0100

    Create aliaser.sh
    Run one time, get very useful aliases.
    You can customize it to add the aliases you want.

---
+++ aliaser.sh
@@ -0,0 +1,5 @@
+echo "## From Aliaser.sh" >> ~/.bashrc
+echo "alias la='ls -alh --color --author'" >> ~/.bashrc
+#echo "alias c='gcc'" >> ~/.bashrc
+echo "alias root='sudo su root'" >> ~/.bashrc
+echo "alias readfile='less'" >> ~/.bashrc
